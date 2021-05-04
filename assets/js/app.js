24ab68e08f016c4e68b35f98ced60a7101c8ee68
Andrew Peterson <petersonaj@outlook.com>
2021-05-03 18:10:46 -0700

    Added parameterized search to the REST API Modified app.js so that it does not interfere with displaying the rest of the website

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
 var app = express();
 
+app.use(express.static("./"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
