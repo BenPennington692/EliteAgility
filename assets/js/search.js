7211bd595312271e71fb3b6fe2b33730c7e4b765
rc1inger <79826138+rc1inger@users.noreply.github.com>
2021-05-07 00:55:43 -0700

    searchBar spacing for singlePage and new doc page

--- assets/js/search.js
+++ assets/js/search.js
@@ -7,8 +7,9 @@ $(document).ready(function () {
 		var result = ""
 		var url = "http://www.omdbapi.com/?apikey="+apikey
 		var movie = $("#searchbar").val()
-		$("#searcharea").css("bottom", "50%")
+		$("#searcharea").css("bottom", "60%")
 		$("#result").addClass('showResult')
+		$(".main-banner .caption").addClass('singlePage')
 		$.ajax({		
 			method:'GET',
 			url:url+"&t="+movie,
@@ -16,13 +17,16 @@ $(document).ready(function () {
 				console.log(data); 
 				result = `
 				<img src="${data.Poster}" width="100%" height="auto" style="float:left" class="img-th poster" />
-				<h2>${data.Title}</h2>
-				<h3>${data.Released}</h3>
-				<h3>${data.Runtime}</h3>
-				<h3>${data.Genre}</h3>
-				<h3>${data.Director}</h3>
-				<h3>${data.Actors}</h3>
+				<div id="movie-info">
+					<h2>${data.Title}</h2>
+					<h3>${data.Released}</h3>
+					<h3>${data.Runtime}</h3>
+					<h3>${data.Genre}</h3>
+					<h3>${data.Director}</h3>
+					<h3>${data.Actors}</h3>
+				</div>
 				`
+				
 				$("#result").empty().append(result)
 				
 			}
